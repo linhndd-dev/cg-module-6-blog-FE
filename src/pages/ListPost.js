9ca93da6d82b2a3e0fee50988663baa1cace0a97
nguyendoanduylinh <nguyen.doan.duy.linh@gmail.com>
2022-10-03 10:02:15 +0700

    fix can le trai

--- src/pages/ListPost.js
+++ src/pages/ListPost.js
@@ -1,7 +1,7 @@
 import { Box, Button, Fab, Pagination, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams,useLocation } from "react-router-dom";
+import { useNavigate, useParams, useLocation } from "react-router-dom";
 import { deletePost, getAllMyPost, getDetailPost } from "../redux/apis";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -22,11 +22,9 @@ import { setCurrentPage } from "../redux/slices/postSlice";
 import { searchMyPosts } from "../redux/apis";
 
 function useQuery() {
-    return new URLSearchParams(useLocation().search);
-  }
+  return new URLSearchParams(useLocation().search);
+}
 export default function ListPost() {
-    
-
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [search, setSearch] = useState("");
@@ -53,14 +51,14 @@ export default function ListPost() {
     } else {
       navigate("/post/list");
     }
-  }
+  };
   const handleDeletePost = async (id) => {
     await dispatch(deletePost(id));
     handleClose();
   };
   const handleShowDetail = async (id) => {
-    navigate(`/post/${id}`)
-}
+    navigate(`/post/${id}`);
+  };
   const handleClickOpen = (id) => {
     setPostId(id);
     setOpen(true);
@@ -70,74 +68,80 @@ export default function ListPost() {
   };
   useEffect(() => {
     dispatch(getAllMyPost(currentPage));
-  }, [currentPage,dispatch]);
+  }, [currentPage, dispatch]);
   const handleEditPost = async (id) => {
     navigate(`/post/edit/${id}`);
   };
   return (
     <Box component="div" sx={{ flexGrow: 1, p: 3 }}>
-      
       <form>
-      <h2>My Posts</h2>
-      <TextField
-      sx={{ minWidth: 650 }}
-        label={"Search Posts"}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <Button variant="contained" size="large" onSubmit={handleSearch}>Search</Button>
+        <h2>My Posts</h2>
+        <TextField
+          sx={{ minWidth: 650 }}
+          label={"Search Posts"}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Button variant="contained" size="large" onSubmit={handleSearch}>
+          Search
+        </Button>
       </form>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
               <TableCell>Avatar</TableCell>
-              <TableCell align="right">Title</TableCell>
-              <TableCell align="right">Summary</TableCell>
-              <TableCell align="right">Like</TableCell>
-              <TableCell align="right">Comment</TableCell>
+              <TableCell align="center">Title</TableCell>
+              <TableCell align="center">Summary</TableCell>
+              <TableCell align="center">Like</TableCell>
+              <TableCell align="center">Comment</TableCell>
               <TableCell align="center" colSpan={2}>
                 Actions
               </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {posts && posts.map((row) => (
-              <TableRow
-                key={row._id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  <img style={{ width: "50px" }} src={`${row.avatar}`} />
-                </TableCell>
-                <TableCell align="right">{row.title}</TableCell>
-                <TableCell align="right">{row.summary}</TableCell>
-                <TableCell align="right">{row.like}</TableCell>
-                <TableCell align="right">{row.comment}</TableCell>
-                <TableCell align="center">
-                  <Fab
-                    color="primary"
-                    aria-label="edit"
-                    onClick={() => handleEditPost(row._id)}
-                  >
-                    <EditIcon />
-                  </Fab>
-                </TableCell>
-                <TableCell align="center">
-                        <Fab color="warning" aria-label="showdetail" onClick={() => handleShowDetail(row._id)}>
-                            <EditIcon />
-                        </Fab>
-                    </TableCell>
-                <TableCell align="center">
-                  <Fab
-                    color="secondary"
-                    aria-label="delete"
-                    onClick={() => handleClickOpen(row._id)}
-                  >
-                    <DeleteIcon />
-                  </Fab>
-                </TableCell>
-              </TableRow>
-            ))}
+            {posts &&
+              posts.map((row) => (
+                <TableRow
+                  key={row._id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    <img style={{ width: "50px" }} src={`${row.avatar}`} />
+                  </TableCell>
+                  <TableCell align="left">{row.title}</TableCell>
+                  <TableCell align="left">{row.summary}</TableCell>
+                  <TableCell align="center">{row.like}</TableCell>
+                  <TableCell align="center">{row.comment}</TableCell>
+                  <TableCell align="center">
+                    <Fab
+                      color="primary"
+                      aria-label="edit"
+                      onClick={() => handleEditPost(row._id)}
+                    >
+                      <EditIcon />
+                    </Fab>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Fab
+                      color="warning"
+                      aria-label="showdetail"
+                      onClick={() => handleShowDetail(row._id)}
+                    >
+                      <EditIcon />
+                    </Fab>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Fab
+                      color="secondary"
+                      aria-label="delete"
+                      onClick={() => handleClickOpen(row._id)}
+                    >
+                      <DeleteIcon />
+                    </Fab>
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
@@ -161,7 +165,11 @@ export default function ListPost() {
         </DialogActions>
       </Dialog>
       <Stack spacing={2}>
-        <Pagination count={numberOfPages ? numberOfPages : 1} color="primary" onChange={handleChangePage} />
+        <Pagination
+          count={numberOfPages ? numberOfPages : 1}
+          color="primary"
+          onChange={handleChangePage}
+        />
       </Stack>
     </Box>
   );
